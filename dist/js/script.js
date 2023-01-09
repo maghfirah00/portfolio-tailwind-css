3cf3a1a53c50668d0b3af7cab1d14e7ddd884f8c
Alan <roalanro@gmail.com>
2023-01-09 15:00:28 +0700

    Update Dark Mode+Perbaikan Fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,58 @@
 //Navbar Fixed
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-    
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
-    }
-}
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
+  }
+};
 
 //Menu
-const menu = document.querySelector('#menu');
-const navMenu = document.querySelector('#nav-menu');
+const menu = document.querySelector("#menu");
+const navMenu = document.querySelector("#nav-menu");
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('menu-active');
-    navMenu.classList.toggle('hidden');
+menu.addEventListener("click", function () {
+  menu.classList.toggle("menu-active");
+  navMenu.classList.toggle("hidden");
 });
+
+//Klik di luar menu
+window.addEventListener("click", function (e) {
+  if (e.target != menu && e.target != navMenu) {
+    menu.classList.remove("menu-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+//Dark Mode
+const checkbok = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+checkbok.addEventListener("click", function () {
+  if (checkbok.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+//Mode Toggel
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  checkbok.checked = true;
+} else {
+  checkbok.checked = false;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
+  darkMode: "class",
   theme: {
-    container:{
+    container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#14b8a6',
-        secondary: '#64748b',
-        dark: '#0f172a',
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
       },
       screen: {
-        '2xl': '1320px',
+        "2xl": "1320px",
       },
       scale: {
-        '-100': '-1',
-      }
+        "-100": "-1",
+      },
     },
   },
   plugins: [],
-}
+};
